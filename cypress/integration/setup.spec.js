afa65c0341475997df0106e6054daec7ad292778
Chris Amico <eyeseast@gmail.com>
2019-08-23 11:22:47 -0400

    Add In-N-Out example and some key controls

--- cypress/integration/setup.spec.js
+++ cypress/integration/setup.spec.js
@@ -1,20 +1,11 @@
 import Ulysses from "../../src/main.js";
 import mapboxgl from "mapbox-gl";
 
-const mapOptions = {
-  container: "#map",
-  accessToken: Cypress.env("MAPBOX_ACCESS_TOKEN"),
-  style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9 // starting zoom
-};
-
 describe("Unit tests for setting up a Ulysses story", function() {
   it("creates a new story", function() {
-    cy.visit("/");
-
-    cy.get("#map")
-      .then(container => new mapboxgl.Map({ mapOptions }))
-      .as("map");
+    cy.visit("/fires").then($window => {
+      expect($window.map).to.be.ok;
+      expect($window.story).to.be.ok;
+    });
   });
 });
