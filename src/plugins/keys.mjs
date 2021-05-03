6b2cf21c740be7e356e384261793b4086e4f7bc9
Chris Amico <eyeseast@gmail.com>
2021-05-03 11:54:32 -0400

    Document plugins

--- src/plugins/keys.mjs
+++ src/plugins/keys.mjs
@@ -1,6 +1,25 @@
 /* plugin for keyboard controls */
-
-export default function keys({ previous = "ArrowLeft", next = "ArrowRight" }) {
+/**
+ * Add keyboard controls to a story. By default, this adds listeners for the
+ * left and right keys, stepping backward and forward, respectively. Using the
+ * numeric keys 0 - 9 will go to those steps.
+ *
+ * Usage:
+ * ```js
+ * import keys from "ulysses/plugins/keys"
+ *
+ * story.use(keys())
+ * ```
+ *
+ * See [Every fire in 2018](./examples/fires.html)
+ *
+ * @export
+ * @param {Object} options [{ previous = "ArrowLeft", next = "ArrowRight" }={}]
+ * @param {String} options.previous Use this key to go to the previous step. Default: `ArrowLeft`
+ * @param {String} options.next Use this key to go to the next step. Default: `ArrowRight`
+ * @returns {Function(Ulysses)}
+ */
+export default function keys({ previous = "ArrowLeft", next = "ArrowRight" } = {}) {
 	function keydown({ key }) {
 		if (key === previous) {
 			story.previous();
